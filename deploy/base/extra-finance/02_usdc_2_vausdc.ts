d31e5534b16f424fb01c6458c8ee659fbbad20da
Rohit Solia <rohit.solia@gmail.com>
2025-07-07 16:29:28 -0500

    Added deploy script for vaUSDC strategies

---
+++ deploy/base/extra-finance/02_usdc_2_vausdc.ts
@@ -0,0 +1,21 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { deployAndConfigureStrategy } from "../../../helpers/deploy-helpers";
+import { EXTRA_FINANCE } from "../../../helpers/deploy-config";
+import Addresses from "../../../helpers/address";
+
+const strategyName = "ExtraFinance_USDC_2";
+
+const func: DeployFunction = async function () {
+  const Address = Addresses.base;
+
+  await deployAndConfigureStrategy({
+    alias: strategyName,
+    contract: EXTRA_FINANCE,
+    proxy: {
+      initializeArgs: [Address.Vesper.vaUSDC, Address.swapper, Address.ExtraFinance.LendingPool, 25, strategyName],
+    },
+  });
+};
+
+func.tags = [strategyName];
+export default func;

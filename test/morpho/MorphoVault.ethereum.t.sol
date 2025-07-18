d0293b63bb3ced125c6c2762d4ced58335f02977
Rohit Solia <rohit.solia@gmail.com>
2025-07-18 15:08:20 -0500

    Added deploy script and tests for Morpho on ETH

--- test/morpho/MorphoVault.ethereum.t.sol
+++ test/morpho/MorphoVault.ethereum.t.sol
@@ -5,15 +5,29 @@ import "forge-std/Test.sol";
 import {Strategy} from "contracts/strategies/Strategy.sol";
 import {MorphoVault} from "contracts/strategies/morpho/MorphoVault.sol";
 import {MorphoVault_Test} from "test/morpho/MorphoVault.t.sol";
-import {SWAPPER, vamsUSD, MORPHO_VAULT_METRONOME_msUSD} from "test/helpers/Address.ethereum.sol";
+import {SWAPPER, vamsUSD, vaUSDC, MORPHO_VAULT_METRONOME_msUSD, MEV_Capital_USDC} from "test/helpers/Address.ethereum.sol";
 import {deinitialize} from "test/helpers/Functions.sol";
 
-contract MorphoVault_Ethereum_Test is MorphoVault_Test {
+contract MorphoVault_Metronome_msUSD_Ethereum_Test is MorphoVault_Test {
     function _setUp() internal override {
         super.createSelectFork("ethereum");
 
         strategy = new MorphoVault();
         deinitialize(address(strategy));
         MorphoVault(address(strategy)).initialize(vamsUSD, SWAPPER, MORPHO_VAULT_METRONOME_msUSD, "");
+    }
+}
+
+contract MorphoVault_MEV_Capital_USDC_Ethereum_Test is MorphoVault_Test {
+    constructor() {
+        MAX_DEPOSIT_SLIPPAGE_REL = 0.0000001e18;
+    }
+
+    function _setUp() internal override {
+        super.createSelectFork("ethereum");
+
+        strategy = new MorphoVault();
+        deinitialize(address(strategy));
+        MorphoVault(address(strategy)).initialize(vaUSDC, SWAPPER, MEV_Capital_USDC, "");
     }
 }
